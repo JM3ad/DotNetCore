93fc352fafb174438c3255c5a9c2364dbb373e70
jack mead <jack-mead@hotmail.co.uk>
2018-11-17 17:48:17 +0000

    New homepage content

--- MeadBros/ClientApp/components/Home.tsx
+++ MeadBros/ClientApp/components/Home.tsx
@@ -1,24 +1,23 @@
-import * as React from 'react';
+﻿import * as React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 
 export default class Home extends React.Component<RouteComponentProps<{}>, {}> {
     public render() {
         return <div>
-            <h1>Hello, world!</h1>
-            <p>Welcome to your new single-page application, built with:</p>
+            <h1>Welcome, Captain of Gondor</h1>
+            <p>They are coming.</p>
+            <p>The enemy have been preparing for war and just as we have been burying spies deep within their ranks, we believe they have also infiltrated our own - choose your compatriots carefully. </p>
+            <p>Osgiliath must hold out the week if we are to give our main forces enough time to prepare for the battles to come - yet our fortifications will not last many frontal assaults. We must use any intelligence we can gather to prepare ambushes to destroy the enemy raiding parties before they can bring their weaponry to bear against our walls.</p>
+
+            <h3>Rules</h3>
             <ul>
-                <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-                <li><a href='https://facebook.github.io/react/'>React</a>, <a href='http://redux.js.org'>Redux</a>, and <a href='http://www.typescriptlang.org/'>TypeScript</a> for client-side code</li>
-                <li><a href='https://webpack.github.io/'>Webpack</a> for building and bundling client-side resources</li>
-                <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-            </ul>
-            <p>To help you get started, we've also set up:</p>
-            <ul>
-                <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-                <li><strong>Webpack dev middleware</strong>. In development mode, there's no need to run the <code>webpack</code> build tool. Your client-side resources are dynamically built on demand. Updates are available as soon as you modify any file.</li>
-                <li><strong>Hot module replacement</strong>. In development mode, you don't even need to reload the page after making most changes. Within seconds of saving changes to files, rebuilt React components will be injected directly into your running application, preserving its live state.</li>
-                <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and the <code>webpack</code> build tool produces minified static CSS and JavaScript files.</li>
-                <li><strong>Server-side prerendering</strong>. To optimize startup time, your React application is first rendered on the server. The initial HTML and state is then transferred to the browser, where client-side code picks up where the server left off.</li>
+                <li>Each day you will receive intelligence relating to the enemies planned assault</li>
+                <li>Share this intelligence with your colleagues and establish the best routes to ambush</li>
+                <li>We only have the forces available to ambush two routes</li>
+                <li>The captains will collectively vote on the best routes to ambush</li>
+                <li>Three direct assaults upon Osgiliath will be all it takes for the enemy to break through our walls</li>
+                <li>Unfortunately, recent activity suggests that the enemy has a spy within our council; one of your colleagues will be spreading disinformation.</li>
+                <li>Find them, and extract the truth from them.</li>
             </ul>
         </div>;
     }
